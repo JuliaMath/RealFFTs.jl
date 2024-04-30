3e747b29c75aaea0caddec2cb10a31636b876960
Tim Holy <tim.holy@gmail.com>
2024-04-29 19:52:24 -0500

    Rename to RealFFTs; require Julia 1.10

--- src/RealFFTs.jl
+++ src/RealFFTs.jl
@@ -1,15 +1,15 @@
 """
-# RFFT
+# RealFFTs
 
-Highlights of the RFFT package:
-- [`RCpair`](@ref): a buffer for in-place real-to-complex FFTs
+Highlights of the RealFFTs package:
+- [`RCpair`](@ref): a buffer for in-place real-to-complex and complex-to-real FFTs
 - [`plan_rfft!`](@ref): create a plan for in-place real-to-complex FFTs
 - [`plan_irfft!`](@ref): create a plan for in-place complex-to-real FFTs
 
 Use `real(RC)` and `complex(RC)` to access the real and complex views of the buffer, respectively.
 `copy!(RC, A)` can be used to copy data (either real- or complex-valued) into the buffer.
 """
-module RFFT
+module RealFFTs
 
 using FFTW, LinearAlgebra
 
